9f96bddff0477ade5eb0b3c582623b1211bbb139
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-05-31 17:40:20 -0500

    Add place holder for  external errorMsg

--- Lab6_Valid/src/main/java/com/students/domain/Phone.java
+++ Lab6_Valid/src/main/java/com/students/domain/Phone.java
@@ -19,8 +19,16 @@ public class Phone implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Min(100)
+	@Max(999)
  	private Integer area;
+	
+	@Min(100)
+	@Max(999)
  	private Integer prefix;
+	
+	@Min(1000)
+	@Max(9999)
  	private Integer number;
 	
 
